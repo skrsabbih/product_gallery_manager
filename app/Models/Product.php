762af1286d7665210c04bf8762b55c8c782805ff
unknown <sabbihkyau@gmail.com>
2026-03-29 15:21:04 +0600

    add eloquent relationships between product and images

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // The attributes that are mass assignable.
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    // Define the relationship with ProductImage
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
 }
--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    //
+    // The attributes that are mass assignable.
+    protected $fillable = [
+        'product_id',
+        'image_path'
+    ];
+
+    // Define the relationship with Product
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
